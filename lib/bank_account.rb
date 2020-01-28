0bfee708178dd3132d41c77bf9e8eb67e3dde8f3
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-28 23:35:45 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,20 @@
 class BankAccount
-
+  attr_reader :name, :balance, :status
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amount)
+    @balance += amount
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+  
+  def valid?
+    
+  end
 end
