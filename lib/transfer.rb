60e49da6936626e660b2fcda42d17300ae8a4427
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-28 23:51:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,20 @@
 class Transfer
   attr_accessor = :sender, :reciever, :amount
+  
   def initialize(sender, recipient, amount)
     @sender = sender
     @reciever = recipient
     @amount = amount
   end
   
+  def sender
+    @sender
+  end
+  
+  def reciever
+    @reciever
+  end
+  
+  def a
   
 end
