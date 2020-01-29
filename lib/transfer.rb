74939f1d472f3457fbfdd5b43c73df4b3358a4ce
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-29 00:13:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,11 @@ class Transfer
   end
   
   def reverse_transfer
-    
-    
+    if self.status == "complete"
+      self.receiver.balance -= @amount
+        self.sender.balance += @amount
+        self.status = "reversed"
+      end
   end
+  
 end
